d5bf93456b320018bc16003a89caff886cb6ffcd
iTimmmy <iTimmyy2000@gmail.com>
2020-04-06 09:36:25 -0400

    TIMMY

--- K_T-Restaurant(JS)/restaurantScript.js
+++ K_T-Restaurant(JS)/restaurantScript.js
@@ -1,6 +1,15 @@
 var theHeader = document.getElementsByClassName("HEADER-BODY");
 var theButton = document.getElementById('TAKE-ME');
+var headerBody = document.querySelector(".HEADER-BODY");
+theButton.addEventListener('click', op);
 
+function op() {
+        headerBody.style.backgroundImage = "url('seaweed.jpg')";
+}
+
+
+
+/*
 var imgSlide = [];
 
 imgSlide[0] = 'home.jpg';
@@ -12,3 +21,4 @@ theHeader.textContent= "ok";
 if (theButton.onclick == true) {
     theButton.value = "Hello";
 }
+*/
