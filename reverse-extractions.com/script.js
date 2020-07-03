ac706bb3bcc5747ce38343a1d4a2082d9931a72a
iTimmmy <iTimmyy2000@gmail.com>
2020-07-03 01:38:45 -0400

    TIMMY

---
+++ reverse-extractions.com/script.js
@@ -0,0 +1,16 @@
+console.log("bitch");
+
+let dropdownAppliances = document.querySelector(".dropdown .appliances li");
+let appliances = document.querySelector(".Appliances");
+console.log("bitch2 ");
+
+
+console.log("bitch3 ");
+
+function dropdown() {
+    console.log("f");
+    dropdownAppliances.style.display = "block";
+}
+
+console.log("bitch4 ");
+
